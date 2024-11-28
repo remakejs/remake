1b4a8fcc4405acb0de453357396b6e2250e73b15
eihigh <eihigh.contact@gmail.com>
2024-11-28 16:46:14 +0900

    simplify example

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,14 +1,15 @@
 import { remake, React } from '@remakejs/remake';
 
-function Counter(p: { parent: Element & { count: number } }) {
-	p.parent.count ||= 0;
-	const incr = () => { p.parent.count++; Counter(p); };
-	remake(p.parent,
+function Counter(p: { parent: Element }) {
+	const root = remake(p.parent, 'div') as any;
+	root.count ||= 0;
+	const incr = () => { root.count++; Counter(p); };
+	remake(root,
 		<>
 			<button onClick={incr}>Increment</button>
-			<div>Count is {p.parent.count}</div>
+			<div>Count is {root.count}</div>
 		</>
 	);
 }
 
-Counter({ parent: document.body as any });
+Counter({ parent: document.body });
